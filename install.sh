8d3dc2e8839b7f44351090e0a7c824eff01789c3
Yang <yangry@vshare.cloud>
2025-06-17 11:51:04 +0000

    Fix Install

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -f $IPFS_PATH/installed ]; then
     echo "IPFS has been installed."
-    ipfs daemon --migrate=true --enable-gc=true --routing=dhtclient
+    /usr/bin/ipfs daemon --migrate=true --enable-gc=true --routing=dhtclient
     exit 0
 fi
 
 # 检查是否已经安装完成
 if [ -f /data/ipfs-install/installed ]; then
     echo "IPFS has been installed."
-    ipfs daemon --migrate=true --enable-gc=true --routing=dhtclient
+    /usr/bin/ipfs daemon --migrate=true --enable-gc=true --routing=dhtclient
     exit 0
 fi
-ipfs init
+/usr/bin/ipfs init
 sh /data/ipfs-install/ipfs-config.sh
 # 安装完成
 touch /data/ipfs-install/installed
 touch $IPFS_PATH/installed
-ipfs daemon --migrate=true --enable-gc=true --routing=dhtclient
+/usr/bin/ipfs daemon --migrate=true --enable-gc=true --routing=dhtclient
 exit 0
